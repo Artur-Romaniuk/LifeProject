fa9e740e3af413e6b92be65cc63ae0471a55e199
“KiczuPL” <KiczuPL@outloo.com>
2021-03-15 13:00:01 +0100

    Gif generating added

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-
+#include <unistd.h>
 
 int main(int argc, char** argv)
 {
@@ -15,17 +15,31 @@ int main(int argc, char** argv)
     matrix_t* CS_Matrix = Load_Matrix(in);  //Current state matrix
     matrix_t* N_CS_Matrix = Make_Matrix(CS_Matrix->rows, CS_Matrix->collumns); //Neighbour curren state matrix
 
-    char buf[32];
+    char buf[64];
 
     if (CS_Matrix == NULL || N_CS_Matrix == NULL)
         return 1;
-    for (int i = 0;i < N;i++) {
+    for (int i = 1;i <= N;i++) {
         Neighbour_counter(CS_Matrix, N_CS_Matrix);
         Rule_applier(CS_Matrix, N_CS_Matrix);
-        sprintf(buf, "gen%d.pbm", i);
+        sprintf(buf, "out/gen%d.pbm", i);
         Write_Matrix_to_PBM(buf, CS_Matrix);
         // Display_Matrix(CS_Matrix);
     }
 
+    //sprintf(buf, "./makegif %d", N);
+    //system(buf);
+    system("mkdir out/tmp");
+    for (int i = 1;i <= N;i++) {
+        sprintf(buf, "ppmtogif -quiet out/gen%d.pbm > out/tmp/gen%d.gif", i, i);
+        system(buf);
+    }
+
+    for (int i = 1;i <= N;i++) {
+        sprintf(buf, "gifsicle -b out/tmp/gen1.gif --append out/tmp/gen%d.gif", i);
+        system(buf);
+    }
+    system("mv out/tmp/gen1.gif out/gen.gif");
+    system("rm -r out/tmp");
     return 0;
 }
